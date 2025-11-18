764a89d1832c5c0d6de84f128bfba645c63e22ff
Rhuan J Souza <rhuan.souza@iesb.edu.br>
2025-11-17 21:50:46 -0300

    feat: endpoints produtos

--- src/middlewares/validateProducto.js
+++ src/middlewares/validateProducto.js
@@ -1,1 +1,19 @@
-a;
+const yup = require('yup');
+
+const validateProduct = async (req, res, next) => {
+    try {
+        const schema = yup.object().shape({
+            name: yup.string().required('Nome do produto é obrigatório'),
+            price: yup.number().positive('Preço deve ser positivo').required('Preço é obrigatório'),
+            category: yup.string().required(),
+            user_id: yup.string().length(24, 'ID do usuário inválido').required('É necessário vincular um usuário (vendedor)')
+        });
+
+        await schema.validate(req.body, { abortEarly: false });
+        next();
+    } catch (error) {
+        return res.status(400).json({ errors: error.errors });
+    }
+};
+
+module.exports = validateProduct;
--- src/models/product.js
+++ src/models/product.js
@@ -1,1 +1,27 @@
-a;
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+    },
+    price: {
+        type: Number,
+        required: true,
+    },
+    category: {
+        type: String,
+        required: true,
+    },
+    // RELACIONAMENTO: Referência ao ID do Usuário (Vendedor)
+    user_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User', // Nome do Model referenciado
+        required: true
+    }
+}, { timestamps: true });
+
+module.exports = mongoose.model('Product', productSchema);
