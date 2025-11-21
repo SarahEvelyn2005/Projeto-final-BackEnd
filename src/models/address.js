c19d06f9434ee557a7a243aa24b38bd73401aa16
Luís Matias <luisvictormatias7@gmail.com>
2025-11-21 19:41:42 -0300

    Refactor address schema to use zipcode and remove user_id

--- src/models/address.js
+++ src/models/address.js
@@ -1,17 +1,25 @@
 const mongoose = require("mongoose");
+
 const AddressSchema = new mongoose.Schema(
   {
-    street: { type: String, required: true },
-    number: { type: String, required: true },
-    city: { type: String, required: true },
-    state: { type: String, required: true },
-    cep: { type: String, required: true },
-    user_id: {
+    user: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
-      required: true,
+      required: true
     },
+    street: { type: String, required: true },
+    number: { type: String, required: true },
+    complement: { type: String },
+    district: { type: String, required: true },
+    city: { type: String, required: true },
+    state: { type: String, required: true },
+    zipcode: {
+      type: String,
+      match: [/^\d{8}$/, "CEP inválido"]
+    },
+    active: { type: Boolean, default: true }
   },
   { timestamps: true }
 );
+
 module.exports = mongoose.model("Address", AddressSchema);
