bbd11a2377570179d62b1185897a71ad5c153eb1
Luís Matias <luisvictormatias7@gmail.com>
2025-11-21 11:03:54 -0300

    Alteração da categoria

--- src/models/category.js
+++ src/models/category.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
-const CategorySchema = new mongoose.Schema(
-  {
-    name: { type: String, required: true },
-    description: { type: String },
-  },
-  { timestamps: true }
-);
+
+const CategorySchema = new mongoose.Schema({
+  name: { type: String, required: true, unique: true },
+  slug: { type: String, required: true, unique: true },
+  description: { type: String }
+}, { timestamps: true });
+
 module.exports = mongoose.model("Category", CategorySchema);
